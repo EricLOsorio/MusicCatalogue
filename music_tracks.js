c391e84f842274332ae1291426d27a81f8509a16
EricLOSorio <erioso@verizon.net>
2017-07-17 16:08:10 -0400

    added ES2015 features to original code

--- music_tracks.js
+++ music_tracks.js
@@ -1,15 +1,19 @@
 window.onload=(function(event){
 
 
-var current=localStorage.getItem('currentGenre');
+//let current=localStorage.getItem('currentGenre');
 
-	var contentSpace=document.getElementsByClassName('content')[0];
+	let contentSpace=document.getElementsByClassName('content')[0];
 
-	var parser = new DOMParser();
-	var fragmentString=localStorage.getItem(current);
-	var parsedDoc= parser.parseFromString(fragmentString, 'text/html');
+	let parser = new DOMParser();
+	let fragmentString=localStorage.getItem('tracks');
+	let parsedDoc= parser.parseFromString(fragmentString, 'text/html');
 
 	contentSpace.appendChild(parsedDoc.documentElement);
+
+
+
+
 	
 
 }(window.console))
